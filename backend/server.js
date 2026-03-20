325fdfae40f54b09ed089b4b209247eb4c48cb99
Sameer <balsindersingh@gmail.com>
2026-03-20 10:30:15 +0530

    Updated frontend with API, location, and search

--- backend/server.js
+++ backend/server.js
@@ -61,3 +61,43 @@ app.listen(PORT, () => {
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
+
+  const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const User = mongoose.model("User", {
+  username: String,
+  email: String,
+  password: String
+});
+
+app.post("/signup", async (req, res) => {
+  const { username, email, password } = req.body;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = new User({
+    username,
+    email,
+    password: hashedPassword
+  });
+
+  await user.save();
+  res.send("User registered");
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) return res.status(400).send("User not found");
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) return res.status(400).send("Wrong password");
+
+  const token = jwt.sign({ id: user._id }, "secretkey");
+
+  res.json({ token });
+});
